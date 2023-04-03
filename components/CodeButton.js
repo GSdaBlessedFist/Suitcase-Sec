5587b6e021ce5f256af26ab2c2d0fb879f1010f8
GSdaBlessedFist <gsdablessedfist@gmail.com>
2023-04-03 17:46:23 -0500

    1)updated Code component link url

--- components/CodeButton.js
+++ components/CodeButton.js
@@ -3,7 +3,7 @@ export default function CodeButton(){
 	return (<>
 		<div className="code-button-component ">
 			<div className="budgeLeft">
-				<a href="https://github.com/GSdaBlessedFist/suitcase-inspired-login-component" className="code-button-link" target="_blank" rel="noreferrer">
+				<a href="https://github.com/GSdaBlessedFist/Suitcase-Sec/blob/main/README.md" className="code-button-link" target="_blank" rel="noreferrer">
 					<div className="code-button-text ">see the code</div>
 				</a>
 			</div>
